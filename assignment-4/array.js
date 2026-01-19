77cfff8451c4f98670e0d2510df15f7acecd1963
Sumit <mdrsumit77@gmail.com>
2026-01-19 13:04:50 +0545

    assignment of array

---
+++ assignment-4/array.js
@@ -0,0 +1,114 @@
+// Q1
+// let arr=[1,2,3,4];
+// arr.push(7);
+// console.log(arr);
+
+
+// Q2
+// let arr=['apple','banana'];
+// arr.pop();
+// console.log(arr);
+
+// Q3
+// let arr = [10, 20, 30, 40];
+// arr.shift();
+// console.log(arr);
+
+// Q4
+// let arr = ['middle', 'end'];
+// arr.unshift('start');
+// console.log(arr);
+
+// Q5
+// let arr = ['one', 'two', 'three'];
+// let str=arr.toString();
+// console.log(str);
+
+// Q6
+// let arr = ['cat', 'dog'];
+// let arr1=['fish', 'bird'];
+// let arr2= arr.concat(arr1);
+// console.log(arr2);
+
+// Q7
+// let arr = ['red', 'green', 'blue'];
+// let newarr= arr.slice(0,2);
+// console.log(newarr);
+
+// Q8
+// let arr = ['x', 'y', 'z'];
+// arr.pop();
+// arr.unshift("a");
+// console.log(arr);
+
+// Q9
+// let arr = [100, 200, 300];
+// let newarr=arr.toString();
+// console.log(newarr.length);
+
+
+// Q10
+// let arr = ['apple', 'banana'];
+// arr.push('orange');
+// arr.push('grape');
+// console.log(arr);
+
+// Q11
+// let arr = ['sun', 'moon', 'stars'];
+// arr.shift();
+// console.log(arr);
+
+// Q12
+// let arr = [7, 14, 21];
+// arr.unshift(42);
+// console.log(arr);
+
+// Q13
+// let arr = [5, 10, 15, 20];
+// let num=arr.pop();
+// console.log(num);
+
+// Q14
+// let arr = ['hello', 'world'];
+// let str = arr.toString();
+// let newarr = str.split(',');
+// console.log(newarr);
+
+// Q15
+// let arr1 = ['x', 'y'];
+// let arr2 = [1, 2];
+// let arr=arr1.concat(arr2);
+// console.log(arr);
+
+
+// Q16
+// let arr = [1, 2, 3, 4];
+// arr.push(99);
+// arr.shift();
+// console.log(arr);
+
+// Q17
+// let arr = ['a', 'b', 'c', 'd'];
+// newArr = arr.slice(2, 5);
+// console.log(newArr);
+
+// Q18
+// let arr = ['elephant','giraffe'];
+// arr.unshift('lion');
+// arr.unshift('tiger');
+// console.log(arr.length);
+
+// Q19
+// let arr = [20, 30, 40];
+// let str = arr.toString();
+// let newArr = str.split(',');
+// console.log(newArr);
+
+// Q20
+// let arr = ['apple', 'banana', 'orange'];
+// arr.push('grape');
+// arr.pop();
+// console.log(arr);
+
+
+
